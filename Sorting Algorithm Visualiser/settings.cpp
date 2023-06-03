aee61c8b5c7c757f5c8a28505fdc4304300b5764
MichaelVerdon <mwverdon2000@gmail.com>
2023-06-03 10:07:06 +0100

    lool

--- Sorting Algorithm Visualiser/settings.cpp
+++ Sorting Algorithm Visualiser/settings.cpp
@@ -1,1 +1,30 @@
-#include "settings.h"
+#include "Settings.h"
+#include <string>
+
+void Settings::setAlgo(std::string algorithm) {
+
+	algorithmChosen = algorithm;
+}
+
+void Settings::setSpeed(int speed) {
+
+	executionSpeed = speed;
+}
+
+void Settings::toggleAudio(bool enabled) {
+
+	audioEnabled = enabled;
+}
+
+std::string Settings::getAlgo() {
+
+	return algorithmChosen;
+}
+
+int Settings::getSpeed() {
+	return executionSpeed;
+}
+
+bool Settings::getAudioEnabled() {
+	return audioEnabled;
+}
--- Sorting Algorithm Visualiser/settings.h
+++ Sorting Algorithm Visualiser/settings.h
@@ -1,10 +1,19 @@
 #pragma once
 #include <string>
 
-class settings
+class Settings
 {
 public:
+
+	void setAlgo(std::string algorithm);
+	void setSpeed(int speed);
+	void toggleAudio(bool enabled);
+	std::string getAlgo();
+	int getSpeed();
+	bool getAudioEnabled();
+private:
+
 	std::string algorithmChosen;
 	int executionSpeed;
-	bool audioEnable;
+	bool audioEnabled;
 };
